aa5aa2b6f6f4888180c9ae26934d092725ef0464
Mohammed Shahid <ofcacc0324@gmail.com>
2022-11-23 23:42:06 +0530

    Udemy     message: "Commit"

--- UdemyHomeWorks/problem3.py
+++ UdemyHomeWorks/problem3.py
@@ -1,12 +1,29 @@
-def master_yoda(string):
-    new_string = string.split()
-    new_string = new_string[::-1]
-    new_string = ' '.join(new_string)
-    return new_string
+def almost_there(num):
+    if num <= 100:
+        if (100 - num) <= 10:
+            return True
+        else:
+            return False
+    elif num <= 111:
+        if (num - 100) <= 10:
+            return True
+        else:
+            return False
+    elif num <= 200:
+        if (200 - num) <= 10:
+            return True
+        else:
+            return False
+    elif num < 211:
+        if (num - 200) <= 10:
+            return True
+        else:
+            return False
 
 
-result_1 = master_yoda('I am home')
-result_2 = master_yoda('We are ready')
+res_1 = almost_there(90)
+res_2 = almost_there(104)
+res_3 = almost_there(150)
+res_4 = almost_there(209)
 
-print(result_1)
-print(result_2)
+print(res_1, res_2, res_3, res_4)
